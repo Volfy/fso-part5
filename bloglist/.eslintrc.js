b00ec3b2e0856a6be5989ae4c394303d1927043a
sasha v <volfym79@gmail.com>
2023-02-11 11:58:50 +0100

    init part5 with bloglist, play

---
+++ bloglist/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 0,
+    semi: [
+      'error',
+      'never',
+    ],
+    'no-console': 0,
+  },
+}
---
+++ bloglist/controllers/blogs.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-underscore-dangle */
+const router = require('express').Router()
+const Blog = require('../models/blog')
+const { userExtractor } = require('../utils/middleware')
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  res.json(blogs)
+})
+
+router.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate('user', { username: 1, name: 1 })
+  res.json(blog)
+})
+
+router.post('/', userExtractor, async (req, res) => {
+  const { body, user } = req
+
+  if (body.title === undefined || body.url === undefined) {
+    return res.status(400).json({ error: 'title or url missing' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes : 0,
+    user: user._id,
+  })
+
+  const saved = await blog.save()
+  user.blogs = user.blogs.concat(saved._id)
+  await user.save()
+
+  return res.status(201).json(saved)
+})
+
+router.delete('/:id', userExtractor, async (req, res) => {
+  const blogId = req.params.id
+  const blog = await Blog.findById(blogId)
+  const { user } = req
+
+  if (!blog) {
+    return res.status(204).end()
+  }
+  if (blog.user.toString() !== user.id.toString()) {
+    return res.status(401).json({ error: 'blog not created by current user' })
+  }
+  await Blog.findByIdAndRemove(blogId)
+  user.blogs = user.blogs.filter((b) => b.toString() !== blogId.toString())
+  await user.save()
+
+  return res.status(204).end()
+})
+
+//
+router.put('/:id', async (req, res) => {
+  const { body } = req
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+  const updated = await Blog.findByIdAndUpdate(req.params.id, blog)
+  return res.json(updated).end()
+})
+
+module.exports = router
